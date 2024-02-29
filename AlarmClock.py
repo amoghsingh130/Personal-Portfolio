2c92d38e63dc42db814ee22359616b97dad090b6
Amogh Singh <125845282+amoghsingh130@users.noreply.github.com>
2024-02-29 15:21:40 -0500

    Update AlarmClock.py

--- AlarmClock.py
+++ AlarmClock.py
@@ -7,27 +7,48 @@ app = gui()
 curr_time = ctime(time()).split()[3]
 window_width = 600
 window_height = 400
-
-clock_border = ImageTk.PhotoImage(Image.open("C:/Users/amogh/OneDrive/Documents/His exellency Amogh Singh/Alarm Clock Project Files/ClockBorder.png"))
+timer_running = True
 
 app.setSize(window_width, window_height)
 
 def update_time():
-    curr_time = ctime(time()).split()[3]
-    if int(curr_time[0:2]) > 12:
-        curr_time = str(int(curr_time[0:2])-12) + curr_time[2:len(curr_time)]
-    app.setLabel("title", f"Time: {curr_time}")
-    app.after(1000, update_time)
+  curr_time = ctime(time()).split()[3]
+  if int(curr_time[0:2]) > 12:
+      curr_time = str(int(curr_time[0:2])-12) + curr_time[2:len(curr_time)]
+  app.setLabel("title", f"Time: {curr_time}")
+  app.after(1000, update_time)
+
+# def stop_timer():
+#   app.setLabel("timer_time", curr_time)
+  
 
 def start_timer():
-    app.setLabel("timer", )
+  timer_running = True
+  while timer_running:
+    hours, minutes, seconds = "0", "0", "0"
+    if len(seconds) == 1: 
+      seconds = "0" + seconds
+    elif seconds == "59": 
+      seconds = "00"
+      minutes = f"{1+int(minutes)}"
+    if len(minutes) == 1: 
+      minutes = "0" + minutes
+    elif minutes == "59": 
+      minutes = "00"
+      hours = f"{1+int(hours)}"
+    seconds = f"{1+int(seconds)}"
+    app.setLabel("timer_time", f"{hours}:{minutes}:{seconds}")
+    curr_label = app.getLabel("timer_time")
+    app.addButton("Stop", running = False)
+    app.after(1000, start_timer)
 
 app.setFont(30)
 app.addLabel("title", f"Time: {curr_time}")
 update_time()
+
 app.addCanvas("canvas")
 
-# app.addButton("startTimer", )
+app.addButton("Start Timer", start_timer)
+app.addLabel("timer_time", "00:00:00")
 
 app.go()
-
